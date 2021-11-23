f02e3104b87b45ecda1ea4f46a6bbe040331cee6
DReyna <reyna_b@upeu.edu.pe>
2021-11-22 19:45:29 -0500

    roles

--- src/main/java/uc/us_security/config/ResourceServerConfig.java
+++ src/main/java/uc/us_security/config/ResourceServerConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/all").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
 		.anyRequest().authenticated()
 		.and().cors().configurationSource(corsConfigurationSource());
 	}
